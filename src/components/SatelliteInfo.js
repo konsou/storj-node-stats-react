9016c43d2209c2e4473aed52f3f0b84a2f507255
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-08-12 10:37:12 +0300

    Remove unneeded line

--- src/components/SatelliteInfo.js
+++ src/components/SatelliteInfo.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import formatStorage from '../utility/formatStorage'
-import hoursInThisMonth from '../utility/storageTime'
 import './SatelliteInfo.css'
 
 const VETTING_AUDITS_NEEDED = 100
